6c13a06a3c9d6b9ca3292fd4dd18ef0a7ae3ea1d
Vargentum <vargentum@gmail.com>
2016-05-18 07:38:46 +0300

    add calculator link to home

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -14,6 +14,10 @@ const Projects = [
   {
     link: 'gardenize',
     name: 'Simple Q/A service for Gardenize junior js developer position'
+  },
+  {
+    link: 'calculator',
+    name: 'Calculator with keyboard support'
   }
 ]
 
