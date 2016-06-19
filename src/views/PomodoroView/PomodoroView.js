22e7e66b2eed1d5dd76cdde5328e85b0e6370765
Vargentum <vargentum@gmail.com>
2016-06-19 18:59:55 +0300

    correct link to github branch

--- src/views/PomodoroView/PomodoroView.js
+++ src/views/PomodoroView/PomodoroView.js
@@ -14,7 +14,7 @@ export class PomodoroView extends Component {
         title="Pomodoro Timer"
         description="Simple apps that implement timers for Pomodoro technique"
         criteriaUrl="https://www.freecodecamp.com/challenges/build-a-pomodoro-clock"
-        sourceCodeUrl="https://github.com/Vargentum/react-redux-apps/tree/pomodoro"
+        sourceCodeUrl="https://github.com/Vargentum/react-redux-apps/tree/new-pomodoro"
         Component={Pomodoro}
       />
     )
